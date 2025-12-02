3c0f72f99e486e1466d7b22856529bd2bd175644
pratiksha <pratiksha.doke@nihilent.com>
2025-12-02 15:21:17 +0530

    added changes

---
+++ Assignment-1/src/main.ts
@@ -0,0 +1,31 @@
+import { bootstrapApplication } from '@angular/platform-browser';
+import { appConfig } from './app/app.config';
+import { App } from './app/app';
+
+bootstrapApplication(App, appConfig)
+  .catch((err) => console.error(err));
+
+
+
+  //Java
+  //module ( package, namespace)
+  //logical collection of java types
+  // class, interface
+
+  //C#
+  //module ( namespace)
+  //logical collection types of C#
+  //class, interface, enum, struct, delegate, events
+  
+  //Typescript
+  //Module
+  //logical colletion angular types
+  //class, interface
+
+  //Typescript along with Angular
+  //Module
+  //Angualr Modules
+  //logical collection of Angular types
+  // components, interface, Injectables, directives, pipes,
+  // interceptors, events, 
+  
